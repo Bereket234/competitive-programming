16388927a6df651f0e16225b3d01450bb0d0a2a0
Bereket234 <bereketnigussie9@gmail.com>
2022-01-12 18:30:51 +0300

    Evaluate Revers Polish

---
+++ week1/week2/EvaluateReversePolishNotation.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+ var evalRPN = function(tokens) {
+    var stack=[]
+    var x=0
+    if (tokens.length<2){
+        return tokens[0]
+    }else{
+        for(var i=0; i< tokens.length; i++){
+         if (tokens[i]=='+' || tokens[i]=='-' || tokens[i]=='*' || tokens[i]=='/'){
+           x= Math.trunc(eval('(' +stack[stack.length-2] + ')' + ''  +tokens[i] + ''+ '('+stack[stack.length-1]+')' ))
+           stack.pop()
+            stack.pop()
+            stack.push(x)
+        }else{
+            stack.push(tokens[i])
+        }
+    }
+    }
+    
+    return (x)
+};
