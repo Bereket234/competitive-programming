32d91acec66d58086de0d22890e01bc67aff4061
Bereket234 <84328067+Bereket234@users.noreply.github.com>
2023-02-28 11:11:54 +0300

    Time: 26 ms (93.85%), Space: 13.9 MB (44.36%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def __init__(self):
+        self.memo= {}
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        if n in self.memo:
+            return self.memo[n]
+        self.memo[n]= self.fib(n - 1) + self.fib(n - 2)
+        return self.memo[n]
