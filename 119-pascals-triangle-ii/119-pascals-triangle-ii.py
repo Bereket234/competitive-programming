2cbeb980c58523b829843292db9817f4682d1641
Bereket234 <84328067+Bereket234@users.noreply.github.com>
2023-03-03 09:48:01 +0300

    Time: 36 ms (41.97%), Space: 13.9 MB (10.52%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.py
@@ -1,10 +1,9 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        curr_sum= [1]
-        
-        for i in range(rowIndex):
-            temp= [0] + curr_sum + [0]
-            curr_sum= []
-            for j in range(len(temp) -1):
-                curr_sum.append(temp[j] + temp[j+1])
+        if rowIndex < 2:
+            return [1] * (rowIndex + 1)
+        temp= [0] + self.getRow(rowIndex-1) + [0]
+        curr_sum= []
+        for j in range(len(temp) -1):
+            curr_sum.append(temp[j] + temp[j+1])
         return curr_sum
