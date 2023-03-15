5593868fbf1aad7cc573fdb99a0ba2100466e6a0
Bereket234 <84328067+Bereket234@users.noreply.github.com>
2023-03-15 13:23:39 +0300

    Time: 134 ms (67.31%), Space: 14.8 MB (28.43%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,26 +1,25 @@
 class Solution:
-    def validate(self, s_cnt, t_cnt):
-        for k in t_cnt:
-            if s_cnt[k] < t_cnt[k]:
-                return False
-        return True
-        
     def minWindow(self, s: str, t: str) -> str:
         s_cnt= defaultdict(int)
         t_cnt= Counter(t)
         res= [-1, len(s) + 2]
         diff= res[1] - res[0]
         l= 0
+        have= 0
         for r in range(len(s)):
             if s[r] in t_cnt:
                 s_cnt[s[r]] += 1
+                if s_cnt[s[r]] == t_cnt[s[r]]:
+                    have+= 1
             
-            while self.validate(s_cnt, t_cnt):
+            while have == len(t_cnt):
                 if diff > r - l + 1:
                     diff= r - l + 1
                     res[0] = l
                     res[1]= r + 1
                 if s[l] in t_cnt:
                     s_cnt[s[l]] -= 1
+                    if s_cnt[s[l]] < t_cnt[s[l]]:
+                        have-=1
                 l+=1
         return "".join(s[res[0]: res[1]]) if diff < len(s) + 2 else ""
