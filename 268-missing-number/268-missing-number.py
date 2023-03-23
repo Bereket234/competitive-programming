188532a0a93433cfa51766ac429ef2b8e01913db
Bereket234 <84328067+Bereket234@users.noreply.github.com>
2023-03-23 18:51:33 +0300

    Time: 167 ms (26.28%), Space: 15.1 MB (67.42%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,10 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        arr_sum= 0
-        for i in nums:
-            arr_sum+=i
-        n= len(nums)
-        range_sum= (n*(1 + n))//2
-        return range_sum - arr_sum
-            
-        
+        nums.append(-1)
+        i= 0
+        while i < len(nums):
+            if nums[i] != i and nums[i] != -1:
+                idx= nums[i]
+                nums[i], nums[idx]= nums[idx], nums[i]
+            else:
+                i+=1
+        return nums.index(-1)
