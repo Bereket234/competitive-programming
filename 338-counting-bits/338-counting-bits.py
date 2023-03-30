62fb2789831a603681b33e725e10de9eae0bcb13
Bereket234 <84328067+Bereket234@users.noreply.github.com>
2023-03-30 22:53:27 +0300

    Time: 132 ms (26.34%), Space: 20.8 MB (65.60%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -4,9 +4,9 @@ class Solution:
         
         for i in range(n+1):
             cnt= 0
-            while i != 0:
-                if i & 1:
-                    cnt +=1
-                i >>= 1
+            while i:
+                cnt += 1
+                i &= i-1
             res.append(cnt)
+            
         return res
