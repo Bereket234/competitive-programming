9f204c9b357853499fa572a2c24b6deb737a84a5
Bereket234 <84328067+Bereket234@users.noreply.github.com>
2023-04-04 21:56:25 +0300

    Time: 123 ms (99.05%), Space: 14.5 MB (5.41%) - LeetHub

--- 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.py
+++ 914-x-of-a-kind-in-a-deck-of-cards/914-x-of-a-kind-in-a-deck-of-cards.py
@@ -1,27 +1,23 @@
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        dict = {}
-        for i in deck:
-            if i in dict: dict[i] += 1
-            else: dict[i] = 1
-
-        from math import gcd
-        def solve(nums):
-            if len(nums) == 1:
-                return nums[0]
-
-            div = gcd(nums[0], nums[1])
-
-            if len(nums) == 2:
-                return div
-
-            for i in range(1, len(nums) - 1):
-                div = gcd(div, nums[i + 1])
-                if div == 1:
-                    return div
-            return div
-
-        hcf = solve(list(dict.values()))
-
-        if hcf > 1: return True
-        else: return False
+        counter= Counter(deck)
+        vals= list(counter.values())
+        gcd= vals[0]
+        
+        def calc_gcd(a, b):
+            a, b = min(a, b), max(a, b)
+            i = a
+            while a > 1 and i >= 2:
+                if a % i == 0 and b % i== 0:
+                    return i
+                i-=1
+            return 1
+        
+        for i in range(1, len(vals)):
+            res= calc_gcd(gcd, vals[i]) 
+            if res == 1:
+                return False
+            gcd= res
+            
+        return gcd != 1
+            
