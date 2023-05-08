67527a9b2ebfd75c49670b3a03d7dcbc5f08bfaf
Bereket234 <84328067+Bereket234@users.noreply.github.com>
2023-05-08 18:42:42 +0300

    Time: 1184 ms (20.21%), Space: 19.4 MB (17.41%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        cnt= {}
+        for i, recipe in enumerate(recipes):
+            cnt[recipe]= len(ingredients[i])
+        
+        for supply in supplies:
+            cnt[supply]= 0
+        
+        graph= defaultdict(list)
+        
+        for i in range(len(ingredients)):
+            for j in range(len(ingredients[i])):
+                graph[ingredients[i][j]].append(recipes[i])
+        
+        que= deque(supplies)
+        
+        while que:
+            node= que.popleft()
+            
+            for child in graph[node]:
+                if cnt[child] != 0:
+                    cnt[child] -= 1
+                if cnt[child] == 0:
+                    que.append(child)
+        print(que, cnt)
+        res= []
+        for recipe in recipes:
+            if cnt[recipe] == 0:
+                res.append(recipe)
+                
+        return res
+                
+            
