59e5381053f65de0ac4519b6784937c74b651917
Bereket234 <84328067+Bereket234@users.noreply.github.com>
2023-10-06 10:03:52 +0300

    Time: 42 ms (85.55%), Space: 16.2 MB (73.99%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,16 +1,26 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
         diff= []
-        for i in range(len(costs)):
-            costa, costb= costs[i]
-            diff.append((costa- costb, i))
-        diff.sort()
-        res= 0
-        for i in range(len(diff) // 2):
-            d, j= diff[i]
-            res += costs[j][0]
+        a_cnt, b_cnt= 0, 0
+        n= len(costs)
         
-        for i in range(len(diff) // 2, len(costs)):
-            d, j= diff[i]
-            res += costs[j][1]
+        for i in range(len(costs)):
+            d= abs(costs[i][0] - costs[i][1])
+            diff.append((d, i))
+        diff.sort(reverse= True)
+        res= 0
+        for d, i in diff:
+            if a_cnt < n//2 and b_cnt < n//2:
+                if costs[i][0] < costs[i][1]:
+                    a_cnt += 1
+                    res += costs[i][0]
+                else:
+                    b_cnt += 1
+                    res += costs[i][1]
+            elif a_cnt == n//2 and b_cnt < n//2:
+                b_cnt += 1
+                res += costs[i][1]
+            else:
+                a_cnt += 1
+                res += costs[i][0]
         return res
